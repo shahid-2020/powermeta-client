64212a602abbae7a90f0be10291e88d122134a4e
shahid <shahid@rapidinnovation.dev>
2021-12-13 00:42:02 +0530

    res nav completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 async function bootstrap() {
+  const icon = document.getElementById('icon')
+  const icon1 = document.getElementById('a')
+  const icon2 = document.getElementById('b')
+  const icon3 = document.getElementById('c')
+  const nav = document.getElementById('nav')
+  const blue = document.getElementById('blue')
+
+  icon.addEventListener('click', function () {
+    icon1.classList.toggle('a')
+    icon2.classList.toggle('c')
+    icon3.classList.toggle('b')
+    nav.classList.toggle('show')
+    blue.classList.toggle('slide')
+  })
+
   const faders = document.querySelectorAll('.fade-in')
   const appearOptions = {
     threshold: 1,
